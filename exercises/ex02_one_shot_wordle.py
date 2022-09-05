2bd252acb0fd5a17adc8261bffa7ac8fe960a119
Suzanne Tyler <97473186+setyler@users.noreply.github.com>
2022-09-05 11:20:27 -0400

    ex 2

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -8,32 +8,31 @@ WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
-while len(str(user_guess)) != len(str(secret_word)):
+while len(user_guess) != len(secret_word):
     user_guess = input(f"That was not {str(len(secret_word))} letters! Try again.")
 
 i: int = 0 
 result: str = "" 
 
-while i < len(str(secret_word)):
+while i < len(secret_word): 
     if secret_word[i] == user_guess[i]: 
-        result = f"{result}{GREEN_BOX}" 
-    else:
-        character_exists = False 
-        i_2: int = i + 1 
-        while character_exists == False and i_2 < len(str(user_guess)): 
-            if secret_word[i_2] == user_guess[i]: 
+        result = f"{result}{GREEN_BOX}"
+    else: 
+        character_exists: bool = False
+        i_2: int = 0
+        while i_2 < len(user_guess) and character_exists == False:
+            if user_guess[i] == secret_word[i_2]:
                 character_exists = True 
-            else: 
-                i_2 += 1
+            i_2 += 1 
         if character_exists == True:
             result = f"{result}{YELLOW_BOX}"
         else:
             result = f"{result}{WHITE_BOX}"
-    i += 1
+    i += 1 
+
 print(result)
 
 if secret_word == user_guess: 
     print("Woo! You got it!") 
 else:
     print("Not quite. Play again soon!")
-# current issues: 'index out of range' in like 5 different places 
