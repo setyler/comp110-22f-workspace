8355e9b1211a05cbe3491f4f78ccd69cf94be55d
Suzanne Tyler <97473186+setyler@users.noreply.github.com>
2022-09-12 09:03:10 -0400

    ex 4

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -6,6 +6,8 @@ def all(input_list: list[int], input_int: int) -> bool:
     """Given a list of integers and a single integers, determines whether all integers in list equal single integer."""
     i: int = 0 
     result = False
+    if len(input_list) == 0:
+        return result
     while i < len(input_list) or result == False:
         if input_list[i] == input_int:
             result = True 
@@ -19,9 +21,9 @@ def all(input_list: list[int], input_int: int) -> bool:
 def max(input: list[int]) -> int:
     """Given a list if integers, returns the largest."""
     i: int = 0
-    result: int = 0
     if len(input) == 0: 
         raise ValueError("max() is an empty List")
+    result: int = list[i]
     if len(input) == 1:
         result = input[0]
         return result 
@@ -36,12 +38,12 @@ def max(input: list[int]) -> int:
 def is_equal(input_1: list[int], input_2: list[int]) -> bool:
     """Given two lists of integers, determines whether each item in each list is equal to one another."""
     result: bool = False
-    message: str = "Looks like one or both of your lists are empty! Try again."
     i: int = 0
     if len(input_1) != len(input_2): 
         return result 
-    if len(input_1) == 0 or len(input_2) == 0:
-        return message
+    if len(input_1) == 0 and len(input_2) == 0:
+        result = True 
+        return result
     while i < len(input_1): 
         if input_1[i] == input_2[i]:
             result = True 
