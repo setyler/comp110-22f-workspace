4ef945bc6a0e0aa930c37996bd8d3f1c29c0a5eb
Suzanne Tyler <97473186+setyler@users.noreply.github.com>
2022-10-01 19:21:29 -0400

    ex06

--- exercises/cyoa.py
+++ exercises/cyoa.py
@@ -3,53 +3,49 @@ __author__ = "730496915"
 
 
 points: int = 0
+player: str = ""
+THEE_NUMBER: int = 0 
 
 
 def main() -> None:
     """Main function for program."""
-    THEE_NUMBER: int = 0
-    user: str = ""
-    greetings() 
+    greet() 
     enter_game: int = 1
-#    enter_game = input(f"To play the game, enter 1. To exit the game, enter 0.")
     while enter_game == 1:
         game_loop()
         enter_game = int(input(f"To play again, enter 1. To exit the game, enter 0."))
     game_over()
 
 
-def greetings() -> None:
+def greet() -> None:
     """Retrieves username and explains rules."""
-    global user 
+    global player 
     user = input(f"Enter your name to get started:")
-    print(f"Welcome to the Number Game, {user}!")
-    print("Game rules are simple. You are trying to guess a secret number between 1 and 100.")
-    print("Game points track how many times you have guessed.")
-    print("You can play an umlimited number of times. Good luck!")
-    return None
+    print(f"Welcome to the Number Game, {player}!")
+    print(f"Game rules are simple. You are trying to guess a secret number between 1 and 100.")
+    print(f"Game points track how many times you have guessed.")
+    print(f"You can play an umlimited number of times. Good luck!")
 
 
 def game_loop() -> None: 
     """Main game loop. One time through = one game played."""
-    global THEE_NUMBER 
     global points 
     from random import randint
     THEE_NUMBER = randint(1, 100)
     guess: int = 0
-    game_over: bool = False
+    game: bool = False
     guess = int(input("I'm thinking of a number between 1 and 100. Enter your first guess."))
-    while game_over == False:
-        if test_guess(guess, THEE_NUMBER) == True:
-            game_over = True
-        if test_guess(guess, THEE_NUMBER) == False:
+    while game:
+        if test_guess(guess, THEE_NUMBER):
+            game = False
+        if not test_guess(guess, THEE_NUMBER):
             if guess > THEE_NUMBER: 
                 guess = int(input("You guessed too high! Guess again."))
             if guess < THEE_NUMBER: 
                 guess = int(input("You guessed too low! Guess again."))
         points += 1
-    if game_over == True: 
-        print(f"Great Job, {user}! You now have {points} points.")
-        return None
+    if game_over: 
+        print(f"Great Job, {player}! You now have {points} points.")
 
     
 def test_guess(guess: int, correct: int) -> bool: 
@@ -63,7 +59,7 @@ def test_guess(guess: int, correct: int) -> bool:
 def game_over() -> None:
     """Print game over message."""
     global points 
-    print(f"Thanks for playing, {user}! You scored {points}.")
+    print(f"Thanks for playing, {player}! You scored {points}.")
 
 
 if __name__ == "__main__":
